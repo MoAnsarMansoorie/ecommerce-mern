5c159fba3e1fd8ef0cb4a5445f7161e81541fe9f
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-06-24 00:19:18 +0530

    setup controller and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// routes for user login
+const loginUser = async (req, res) => {
+
+}
+
+// routes for user registration
+const registerUser = async (req, res) => {
+
+}
+
+// route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin };
