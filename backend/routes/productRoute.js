40cba95115168b96ea320385e34c9755c9b67f4d
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-06-25 23:45:17 +0530

    created productRoute and multer

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { addProducrController, listProducrController, removeProducrController, singleProducrController } from '../controllers/productController.js';
+import upload from '../middlewares/multer.js';
+
+const router = express.Router();
+
+// Route for add product
+router.post('/add', upload.fields([{ name: "image1", maxCount: 1 }, { name: "image2", maxCount: 1 }, { name: "image3", maxCount: 1 }, { name: "image4", maxCount: 1 },]), addProducrController);
+// Route for list product
+router.get('/list', listProducrController);
+// Route for remove product
+router.post('/remove/:id', removeProducrController);
+// Route for single product info
+router.post('/single/:id', singleProducrController);
+
+export default router;
