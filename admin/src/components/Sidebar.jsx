390a6bb6687bb0c6804bc9e104bba486dd704c6f
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-07-25 00:31:38 +0530

    create list page with backend integration

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
           <p className='hidden md:block'>Add Items</p>
         </NavLink>
 
-        <NavLink to="/list-items" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1">
+        <NavLink to="/list" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1">
           <img className='w-5 h-5' src={assets.order_icon} alt="Logo" />
           <p className='hidden md:block'>List Items</p>
         </NavLink>
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { backendUrl } from '../App'; // Import the backend URL from App.jsx
+import { toast } from 'react-toastify';
+import { currency } from '../App';
 
-const List = () => {
-  return (
-    <div>
+const List = ({token}) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/api/v1/product/list`)
+      console.log(response.data)
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
       
-    </div>
+    } catch (error) {
+      console.error("Error fetching list:", error);
+      toast.error(error.message)
+      
+    }
+
+  }
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(`${backendUrl}/api/v1/product/remove/${id}`, {id}, {
+        headers: { token }
+      });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList(); // Refresh the list after deletion
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error("Error deleting product:", error);
+      toast.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  },[])
+
+  return (
+    <>
+      <p>All Products List</p>
+
+      <div className='flex flex-col gap-2'>
+
+        {/* All Products Heading */}
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] gap-4 py-1 px-2 bg-gray-100 text-sm'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        {/* All Products List Data */}
+        {
+          list.map((item, index) => (
+            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-5 px-2 py-1 border text-sm' key={index} >
+              <img src={item.image[0]} alt={item.name} className='w-12' />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{currency}{item.price}</p>
+              <p onClick={() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
